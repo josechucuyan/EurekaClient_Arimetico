efbbc29335d83dc132d6588c5d09b6fe057a936a
unknown <josedachu@gmail.com>
2022-02-07 13:44:15 +0100

    Cliente eureka

---
+++ eureka_arimetico/src/test/java/com/example/eureka_arimetico/EurekaArimeticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_arimetico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaArimeticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
